8c839183fa90dac39267a8de9ffb99a39a09ebd4
Paul <paul.deterrassondemontleau@swisstxt.ch>
2024-07-16 17:58:57 +0200

    Git — Add .gitconfig and .gitignore

---
+++ install.bat
@@ -0,0 +1,53 @@
+@echo off
+setlocal
+
+rem Check for administrative rights
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo.
+    echo *** This script requires administrative privileges. ***
+    echo *** Please right-click the script and select "Run as administrator". ***
+    pause
+    exit /b 1
+)
+
+set "script_path=%~dp0"
+
+call :backup_and_link "%script_path%.gitconfig" "%userprofile%\.gitconfig"
+call :backup_and_link "%script_path%.gitignore.global" "%userprofile%\.gitignore.global"
+call :backup_and_link "%script_path%.gitconfig.user" "%userprofile%\.gitconfig.user"
+
+pause
+
+rem ----------------------------
+rem    End of the main body
+rem ----------------------------
+
+
+:backup_and_link
+set "src=%~1"
+set "dest=%~2"
+
+rem Check if destination exists and is a symlink
+if exist "%dest%" (
+    fsutil reparsepoint query "%dest%" >nul 2>&1
+    if %errorlevel% equ 0 (
+        echo Removing symlink %dest%
+        del "%dest%"
+    ) else (
+        echo File %dest% already existed, backing it up to %dest%.bak...
+        move "%dest%" "%dest%.bak"
+    )
+)
+
+echo Creating symlink %dest% to %src%
+mklink "%dest%" "%src%"
+if %errorlevel% neq 0 (
+    echo Failed to create symlink %dest% to %src%
+)
+
+echo:
+
+goto :eof
+
+endlocal
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -eu
+
+backup_and_link() {
+    src="$1"
+    dest="$2"
+
+    if [ -L "$dest" ]; then
+        echo "Removing symlink $dest"
+        rm "$dest"
+    elif [ -e "$dest" ]; then
+        echo "File $dest already exists and will be backed up to $dest.bak"
+        mv "$dest" "$dest.bak"
+    fi
+
+    echo "Creating symlink $dest → $src"
+    ln -s "$src" "$dest"
+    echo
+}
+
+backup_and_link "$(pwd)/.gitconfig" "$HOME/.gitconfig"
+backup_and_link "$(pwd)/.gitignore.global" "$HOME/.gitignore.global"
+backup_and_link "$(pwd)/.gitconfig.user" "$HOME/.gitconfig.user"
